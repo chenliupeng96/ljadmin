255103923af3cb33fc5ca6dd4947cfe98ea0d774
chenliupeng96 <clp9627@163.com>
2020-06-25 04:22:11 +0800

    添加router-vue 并 添加文件夹views

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
 
+// vue-router
+import router from './router';
+
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
