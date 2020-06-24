a4cb12f519b7b390693ed54f63189799925d3f2d
chenliupeng96 <clp9627@163.com>
2020-06-25 06:04:05 +0800

    登录UI界面构建

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,12 +1,41 @@
 <template>
   <div>
-    登录
+    <div class="container">
+      <div class="row">
+        <!-- 12 -->
+        <div class="col-12 col-sm-9 col-md-7 col-lg-5 m-auto pt-5" style="height:500px;">
+          <div class="card mt-5">
+            <div class="card-header bg-white">
+              <h3 class="text-center mb-0 text-secondary">LJ-ADMIN</h3>
+            </div>
+            <div class="card-body">
+              <el-form ref="form" :model="form">
+                <el-form-item>
+                  <el-input v-model="form.username" size="medium" placeholder="请输入用户名"></el-input>
+                  <el-input v-model="form.password" size="medium" placeholder="请输入密码" type="password"></el-input>
+                </el-form-item>
+                <el-form-item>
+                  <el-button type="primary" class="w-100">立即创建</el-button>
+                </el-form-item>
+              </el-form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    
+    data(){
+      return{
+        form:{
+          username:'',
+          password:''
+        }
+      }
+    }
   }
 </script>
 
