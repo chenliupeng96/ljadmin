f6c4a9b609283ce020a79a5b6e67d6f42e385b8d
chenliupeng96 <clp9627@163.com>
2020-06-25 19:51:24 +0800

    顶部导航开发1

--- src/views/layout.vue
+++ src/views/layout.vue
@@ -1,15 +1,42 @@
 <template>
   <div>
-    <el-container style="position: absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;">
-      <el-header>Header</el-header>
+    <el-container
+      style="position: absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;"
+    >
+      <el-header class="d-flex align-items-center" style="background-color:#545c64;">
+        <!-- logo -->
+        <a class="h5 text-light mb-0 mr-auto">LJ-ADMIN</a>
+        <el-menu
+          :default-active="navBarIndex"
+          mode="horizontal"
+          @select="handleSelect"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-menu-item index="2">商品</el-menu-item>
+          <el-menu-item index="3">订单</el-menu-item>
+          <el-menu-item index="4">会员</el-menu-item>
+          <el-menu-item index="5">设置</el-menu-item>
+           <el-submenu index="100">
+            <template slot="title">
+              <el-avatar size="small" src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"></el-avatar>
+              tail
+              </template>
+            <el-menu-item index="100-1">修改</el-menu-item>
+            <el-menu-item index="100-2">退出</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-header>
       <el-container style="height:100%;padding-bottom:60px;">
         <!-- 侧边布局 -->
         <el-aside width="200px">
-          <li v-for="i in 100" :key="i">{{i}}</li>
+           <li v-for="i in 100" :key="i">{{ i }}</li>
         </el-aside>
         <!-- 主布局 -->
         <el-main>
-          <li v-for="i in 100" :key="i">{{i}}</li>
+          <li v-for="i in 100" :key="i">{{ i }}</li>
         </el-main>
       </el-container>
     </el-container>
@@ -19,21 +46,20 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      navBarIndex: "1",
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.el-header {
-  background-color: #b3c0d1;
-  color: #333;
-}
- .el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-  }
-  
-  .el-main {
-    background-color: #E9EEF3;
-    color: #333;
-  }
+
 </style>
