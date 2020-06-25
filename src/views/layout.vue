5384baee3c6ccf6585ce800c8fcf4df2965dad44
chenliupeng96 <clp9627@163.com>
2020-06-25 19:24:18 +0800

    主布局开发

--- src/views/layout.vue
+++ src/views/layout.vue
@@ -1,16 +1,39 @@
 <template>
   <div>
-    主布局
-    <router-view></router-view>
+    <el-container style="position: absolute;left:0;top:0;right:0;bottom:0;overflow:hidden;">
+      <el-header>Header</el-header>
+      <el-container style="height:100%;padding-bottom:60px;">
+        <!-- 侧边布局 -->
+        <el-aside width="200px">
+          <li v-for="i in 100" :key="i">{{i}}</li>
+        </el-aside>
+        <!-- 主布局 -->
+        <el-main>
+          <li v-for="i in 100" :key="i">{{i}}</li>
+        </el-main>
+      </el-container>
+    </el-container>
+
+    <!-- <router-view></router-view> -->
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {};
 </script>
 
 <style lang="scss" scoped>
-
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+}
+ .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+  }
 </style>
